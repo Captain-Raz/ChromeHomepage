0addbe5f3570d0de1384982cf179cab3d59afa77
Captain-Raz <75558285+Captain-Raz@users.noreply.github.com>
2021-02-03 10:32:49 -0600

    Add files via upload

---
+++ Github Scripts.js
@@ -0,0 +1,77 @@
+function lorem1() {
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+};
+
+function lorem2() {
+    window.open(
+      "http://www.google.com", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "http://www.bing.com", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "http://www.yahoo.com", "_blank", "noreferrer noopener"
+    );
+};
+
+function lorem3() {
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+};
+
+function lorem4() {
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+};
+
+function lorem5() {
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+};
+
+function lorem6() {
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+    window.open(
+      "", "_blank", "noreferrer noopener"
+    );
+};
